ce21d491e53be8097c3d4a050277563b034c6fa3
unknown <brian.zhen101@gmail.com>
2024-07-12 14:18:15 -0400

    temp console log

---
+++ assets/js/grubpage.js
@@ -0,0 +1,12 @@
+const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/9.3.2' } };
+var temp = "dodgers"
+fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${temp}i%20&apikey=yGPLSRwLKzHAgAdFANWvvUHIwVbZm8o3`, options)
+    .then(response => response.json())
+  
+
+    .then(response => console.log(response))
+
+
+
+
+    .catch (err => console.error(err)) 
