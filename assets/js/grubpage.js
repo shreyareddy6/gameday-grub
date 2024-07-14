0c0ffdaa4f7313751d8f32f2457ad8b9657a7931
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-14 15:36:14 -0400

    added function to fetch venue api data

--- assets/js/grubpage.js
+++ assets/js/grubpage.js
@@ -1,24 +1,28 @@
-const ticketmaster = { method: 'GET', headers: { 'User-Agent': 'insomnia/9.3.2' } };
-var temp = "dodgers"
-fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${temp}%20&apikey=yGPLSRwLKzHAgAdFANWvvUHIwVbZm8o3`, ticketmaster)
-    .then(response => response.json())
+const apiKey = 'yGPLSRwLKzHAgAdFANWvvUHIwVbZm8o3'
+// user input values for ticketmaster api query to get venue data
+const teamName = 'dodgers'
+const gameDate = '2024-07-19'
+// const gameDate = document.getElementById('').value;
+// const teamName = document.getElementById('').value;
 
-    .then(response => console.log(response))
+// a function to fetch the gameday venue data from the api
+function getVenueApi() {
+    const ticketmasterUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=${apiKey}&keyword=${teamName}&localStartEndDateTime=${gameDate}T00:00:00,${gameDate}T23:59:59`
 
-    .catch(err => console.error(err))
+    fetch(ticketmasterUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            console.log(data._embedded.events[0]._embedded.venues[0].name)
+            console.log(data._embedded.events[0]._embedded.venues[0].address)
+            console.log(data._embedded.events[0]._embedded.venues[0].city)
+            console.log(data._embedded.events[0]._embedded.venues[0].state)
+            console.log(data._embedded.events[0]._embedded.venues[0].location)
+        })
+        .catch(err => console.error(err))
+}
 
-const url = 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=304554';
-const tripadvisor16 = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': '610f35df02msh62c05973662210dp1fb3e6jsn1221486feec9',
-        'x-rapidapi-host': 'tripadvisor16.p.rapidapi.com'
-    }
-};
 
-fetch(`https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=304554`, tripadvisor16)
-    .then(response => response.json())
-
-    .then(response => console.log(response))
-
-    .catch(err => console.error(err))
+getVenueApi();
