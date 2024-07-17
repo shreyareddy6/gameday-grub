ded0c12dd75df19e1092b6c4e4f8be108a082503
shreyareddy <32663313+shreyareddy6@users.noreply.github.com>
2024-07-16 23:57:49 -0400

    changes to restaurant cards

--- assets/js/grubpage-script.js
+++ assets/js/grubpage-script.js
@@ -58,13 +58,13 @@ async function nearbySearch() {
 }
 
 async function renderRestaurantCard() {
-  let places = await nearbySearch()
+  let places = await nearbySearch();
 
   for (let i = 0; i < places.length; i++) {
     const temp = places[i];
     const restaurant = temp.Eg.photos[0].name;
-    const test = restaurant.split("photos/")
-    const photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=200&photo_reference=${test[1]}&key=AIzaSyAmPbsK8vlRNrUfQKgY9QM-yI-WZanhKBY`
+    const test = restaurant.split("photos/");
+    const photoURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=200&photo_reference=${test[1]}&key=AIzaSyAmPbsK8vlRNrUfQKgY9QM-yI-WZanhKBY`;
 
     console.log(photoURL);
     console.log(test);
@@ -73,49 +73,62 @@ async function renderRestaurantCard() {
 
     console.log(temp);
 
-    const div1 = document.createElement('div');
-    div1.classList.add("w-96", "mx-auto", "bg-white", "rounded-xl", "shadow-md", "overflow-hidden", "border-2", "border-orange-300");
+    const aTagCard = document.createElement("a");
+    aTagCard.href = temp.Eg.websiteURI;
 
-    const img = document.createElement('img');
+    const div1 = document.createElement("div");
+    div1.classList.add(
+      "w-96",
+      "mx-auto",
+      "bg-white",
+      "rounded-xl",
+      "shadow-md",
+      "overflow-hidden",
+      "border-2",
+      "border-orange-300"
+    );
+
+    const img = document.createElement("img");
     img.classList.add("w-full", "h-48", "object-cover");
-    img.setAttribute('src', photoURL);
+    img.setAttribute("src", photoURL);
 
-    const div2 = document.createElement('div');
+    const div2 = document.createElement("div");
     div2.classList.add("p-6");
 
-    const h = document.createElement('h2');
-    h.classList.add('text-lg', 'font-bold', 'text-black');
+    const h = document.createElement("h2");
+    h.classList.add("text-lg", "font-bold", "text-black");
     h.textContent = temp.Eg.displayName;
 
-    const p = document.createElement('p');
-    p.classList.add('mt-1', 'font-thin', 'text-black')
+    const p = document.createElement("p");
+    p.classList.add("mt-1", "font-thin", "text-black");
     p.textContent = temp.Eg.formattedAddress;
 
-    const div3 = document.createElement('div');
-    div3.classList.add('mt-4', 'flex', 'items-center', 'flex-col');
+    const div3 = document.createElement("div");
+    div3.classList.add("mt-4", "flex", "items-center", "flex-col");
 
-    const div4 = document.createElement('div');
-    div4.classList.add('flex')
+    const div4 = document.createElement("div");
+    div4.classList.add("flex");
 
-    const rating = document.createElement('p');
-    rating.classList.add('mt-1', 'text-black');
+    const rating = document.createElement("p");
+    rating.classList.add("mt-1", "text-black");
     rating.textContent = `⭐️ Rating: ${temp.Eg.rating}`;
 
     //if (temp.Eg.websiteURI !== null) {
-      const a = document.createElement('a');
-      a.classList.add('block', 'mt-4', 'text-indigo-500', 'hover:underline');
-      a.textContent = temp.Eg.websiteURI;
+    // const a = document.createElement('a');
+    // a.classList.add('block', 'mt-4', 'text-indigo-500', 'hover:underline');
+    // a.textContent = temp.Eg.websiteURI;
     //}
 
-    card.append(div1);
+    card.append(aTagCard);
+    aTagCard.append(div1);
     div1.appendChild(img);
     div1.appendChild(div2);
     div2.appendChild(h);
     div2.appendChild(p);
     div2.appendChild(div3);
     div3.appendChild(div4);
-    div3.appendChild(a);
-    div4.appendChild(rating)
+    // div3.appendChild(a);
+    div4.appendChild(rating);
   }
 }
 
